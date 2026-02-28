11938b9c1c1cc3863291c2a9e60dd9d2d665b548
Hijran9999 <119710869+Hijran9999@users.noreply.github.com>
2026-02-28 17:00:08 -0500

    new changes

--- src/com/example/App.java
+++ src/com/example/App.java
@@ -1,19 +1,3 @@
-//
-//public class App {
-//    public static void main(String[] args) {
-//        System.out.println("Hello SonarQube Demo!");
-//
-//        // Intentional issue: unused variable
-//        int unusedVariable = 42;
-//
-//        // Intentional bug: empty catch block
-//        try {
-//            int result = 10 / 0;
-//        } catch (Exception e) {
-//            // TODO: handle exception
-//        }
-//    }
-//}
 package com.example; // Best practice: always use a package
 
 import java.util.logging.Logger;
